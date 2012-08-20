307079a0edeb2376182be36bf5c020036b40dc68
marsiggia001@gmail.com <marsiggia001@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-20 03:18:41 +0000

    Merge branch 'crudArea'

--- src/br/com/fiap/clinica/bean/AreaBean.java
+++ src/br/com/fiap/clinica/bean/AreaBean.java
@@ -1,26 +1,78 @@
 package br.com.fiap.clinica.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
 
 import br.com.fiap.clinica.dao.DAO;
+import br.com.fiap.clinica.dao.JPAUtil;
 import br.com.fiap.clinica.modelo.Area;
 
 @ManagedBean
-public class AreaBean {
-
+@ViewScoped
+public class AreaBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	private Area area = new Area();
+	private EntityManager em = new JPAUtil().getEntityManager();
+	DAO<Area> dao = new DAO<Area>(Area.class, em);
+	
+	private List<Area> areas;
+	
+	public AreaBean() {
+		limpaForm();
+	}
 	
 	public Area getArea() {
 		return this.area;
 	}
 	
 	public void grava() {
-		DAO<Area> dao = new DAO<Area>(Area.class);
-		dao.adiciona(area);
+		try{
+			em.getTransaction().begin();
+			
+			if (this.area.getId() == null) {			
+				dao.adiciona(area);
+			}else{			
+				dao.atualiza(area);
+			}
+			
+			em.getTransaction().commit();
+		}catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		limpaForm();
 	}
 	
-	public void altera() {
-		DAO<Area> dao = new DAO<Area>(Area.class);
-		dao.atualiza(area);
+	public void limpaForm(){
+		this.area = new Area();
+		this.areas = dao.lista();
+	}
+	
+	public List<Area> getAreas(){
+		return areas;
+	}
+	
+	public void edita(Area aEditar){
+		this.area = aEditar;
+	}
+	
+	public void remove(Area aRemover){
+		try{
+			em.getTransaction().begin();
+			
+			this.area = dao.buscaPeloId(aRemover.getId()); 
+			dao.remove(this.area);			
+			
+			em.getTransaction().commit();
+		}catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		limpaForm();
 	}
 }
--- src/br/com/fiap/clinica/modelo/Area.java
+++ src/br/com/fiap/clinica/modelo/Area.java
@@ -7,14 +7,13 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="area")
 public class Area implements Serializable {
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue
