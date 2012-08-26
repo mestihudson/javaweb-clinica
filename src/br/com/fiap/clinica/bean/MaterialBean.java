893a4cecadb73b2ea7e3d3f3121373316b2bd525
marsiggia001@gmail.com <marsiggia001@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-26 20:05:15 +0000

    CrudMaterialMedicamento, insert, update, delete e select

--- src/br/com/fiap/clinica/bean/MaterialBean.java
+++ src/br/com/fiap/clinica/bean/MaterialBean.java
@@ -1,29 +1,101 @@
 package br.com.fiap.clinica.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
 
 import br.com.fiap.clinica.dao.DAO;
 import br.com.fiap.clinica.dao.JPAUtil;
 import br.com.fiap.clinica.modelo.Material;
+import br.com.fiap.clinica.modelo.MaterialTipo;
 
-@ManagedBean
-public class MaterialBean {
-
-	private Material material = new Material();
+@ManagedBean	
+@ViewScoped
+public class MaterialBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Material material;
 	private EntityManager em = new JPAUtil().getEntityManager();
-	DAO<Material> dao = new DAO<Material>(Material.class, em);
+	private DAO<Material> dao = new DAO<Material>(Material.class, em);
+	private DAO<MaterialTipo> daoTipoMaterial= new DAO<MaterialTipo>(MaterialTipo.class, em);
+	
+	private List<Material> materiais;
+	private List<MaterialTipo> tiposMateriais;
+	
+	private MaterialTipo tipo;
+	
+	public MaterialBean() {
+		limpaForm();
+	}
 	
 	public Material getMaterial() {
 		return this.material;
 	}
 	
-	public void grava() {
-		dao.adiciona(material);
+	public List<MaterialTipo> getTiposMateriais(){
+		return this.tiposMateriais;
 	}
 	
-	public void altera() {
-		dao.atualiza(material);
+	public MaterialTipo getTipo(){
+		return this.tipo;
 	}
-
+	
+	public void setTipo(MaterialTipo tipo){
+		this.tipo = tipo;
+	}
+	
+	public void grava() {
+		try{
+			em.getTransaction().begin();
+			
+			this.material.setMaterialTipo(daoTipoMaterial.buscaPeloId(this.tipo.getId())); 
+			if (this.material.getId() == null) {			
+				dao.adiciona(material);
+			}else{
+				dao.atualiza(material);
+			}
+			
+			em.getTransaction().commit();
+		}catch (Exception e) {
+			em.getTransaction().rollback();
+			throw e;
+		}
+		
+		limpaForm();
+	}
+	
+	public void limpaForm(){
+		this.material = new Material();
+		this.materiais = dao.lista();
+		this.tiposMateriais = daoTipoMaterial.lista();
+		this.tipo = new MaterialTipo();
+	}
+	
+	public List<Material> getMateriais(){
+		return materiais;
+	}
+	
+	public void edita(Material aEditar){
+		this.material = aEditar;
+		this.tipo.setId(aEditar.getMaterialTipo().getId());
+	}
+	
+	public void remove(Material aRemover){
+		try{
+			em.getTransaction().begin();
+			
+			this.material = dao.buscaPeloId(aRemover.getId()); 
+			dao.remove(this.material);			
+			
+			em.getTransaction().commit();
+		}catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		limpaForm();
+	}	
 }
