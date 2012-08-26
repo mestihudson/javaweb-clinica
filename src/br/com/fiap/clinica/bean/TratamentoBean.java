ac9cdea2cf75e02f387b13ba75f25367238c7c2a
marsiggia001@gmail.com <marsiggia001@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-26 20:58:17 +0000

    CrudTratamento, insert, update, delete e select

--- src/br/com/fiap/clinica/bean/TratamentoBean.java
+++ src/br/com/fiap/clinica/bean/TratamentoBean.java
@@ -1,26 +1,78 @@
 package br.com.fiap.clinica.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
 
 import br.com.fiap.clinica.dao.DAO;
+import br.com.fiap.clinica.dao.JPAUtil;
 import br.com.fiap.clinica.modelo.Tratamento;
 
 @ManagedBean
-public class TratamentoBean {
-
+@ViewScoped
+public class TratamentoBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
 	private Tratamento tratamento = new Tratamento();
+	private EntityManager em = new JPAUtil().getEntityManager();
+	DAO<Tratamento> dao = new DAO<Tratamento>(Tratamento.class, em);
+	
+	private List<Tratamento> tratamentos;
+	
+	public TratamentoBean() {
+		limpaForm();
+	}
 	
 	public Tratamento getTratamento() {
 		return this.tratamento;
 	}
 	
 	public void grava() {
-		DAO<Tratamento> dao = new DAO<Tratamento>(Tratamento.class);
-		dao.adiciona(tratamento);
+		try{
+			em.getTransaction().begin();
+				
+			if (this.tratamento.getId() == null) {			
+				dao.adiciona(tratamento);
+			}else{			
+				dao.atualiza(tratamento);
+			}
+			
+			em.getTransaction().commit();
+		}catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		limpaForm();
 	}
 	
-	public void altera() {
-		DAO<Tratamento> dao = new DAO<Tratamento>(Tratamento.class);
-		dao.atualiza(tratamento);
+	public void limpaForm(){
+		this.tratamento = new Tratamento();
+		this.tratamentos = dao.lista();
+	}
+	
+	public List<Tratamento> getTratamentos(){
+		return tratamentos;
+	}
+	
+	public void edita(Tratamento aEditar){
+		this.tratamento = aEditar;
+	}
+	
+	public void remove(Tratamento aRemover){
+		try{
+			em.getTransaction().begin();
+			
+			this.tratamento = dao.buscaPeloId(aRemover.getId()); 
+			dao.remove(this.tratamento);			
+			
+			em.getTransaction().commit();
+		}catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		
+		limpaForm();
 	}
 }
