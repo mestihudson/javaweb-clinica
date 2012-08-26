59d8c5e78cf590af82a5f1048b596ee907cea2c4
gianpaulosoares@gmail.com <gianpaulosoares@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-26 20:51:32 +0000

    iniciando cadastro de médicos

--- src/br/com/fiap/clinica/bean/MedicoBean.java
+++ src/br/com/fiap/clinica/bean/MedicoBean.java
@@ -1,27 +1,69 @@
 package br.com.fiap.clinica.bean;
 
+import java.io.Serializable;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
 
 import br.com.fiap.clinica.dao.DAO;
+import br.com.fiap.clinica.dao.JPAUtil;
+import br.com.fiap.clinica.modelo.MaterialTipo;
 import br.com.fiap.clinica.modelo.Medico;
 
 @ManagedBean
-public class MedicoBean {
+public class MedicoBean implements Serializable {
 
-	private Medico medico = new Medico();
+	private Medico medico;
+	private EntityManager em = new JPAUtil().getEntityManager();
+	private DAO<Medico> dao = new DAO<Medico>(Medico.class, em);
+	
+	private List<Medico> medicos;
+	
+	public MedicoBean() {
+		limpaForm();
+	}
+	
+	public void limpaForm() {
+		this.medico = new Medico();
+	}
 	
 	public Medico getMedico() {
 		return this.medico;
 	}
 	
 	public void grava() {
-		DAO<Medico> dao = new DAO<Medico>(Medico.class);
-		dao.adiciona(medico);
+		try {
+			em.getTransaction().begin();
+			if (this.medico.getId() == null){
+				dao.adiciona(medico);
+			} else {
+				dao.atualiza(medico);
+			}
+			
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			limpaForm();
+		}
 	}
 	
 	public void altera() {
-		DAO<Medico> dao = new DAO<Medico>(Medico.class);
 		dao.atualiza(medico);
 	}
 	
+	public void edita(Medico aEditar){
+		this.medico = aEditar;
+	}
+	
+	public void remove(Medico aRemover){
+		try {
+			em.getTransaction().begin();
+			this.medico = dao.buscaPeloId(aRemover.getId());
+			dao.remove(this.medico);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			em.getTransaction().rollback();
+		}
+		limpaForm();
+	}
 }
