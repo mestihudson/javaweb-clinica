6415f620fdd8b821c565b4453eecf9440df0d662
gianpaulosoares@gmail.com <gianpaulosoares@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-27 04:47:33 +0000

    atendimento inacabado, espero conseguir acabar essa merda amanhã na aulagit st

--- src/br/com/fiap/clinica/bean/AtendimentoBean.java
+++ src/br/com/fiap/clinica/bean/AtendimentoBean.java
@@ -5,11 +5,16 @@ import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import br.com.fiap.clinica.dao.DAO;
 import br.com.fiap.clinica.dao.JPAUtil;
 import br.com.fiap.clinica.modelo.Agendamento;
 import br.com.fiap.clinica.modelo.Atendimento;
+import br.com.fiap.clinica.modelo.AtendimentoMaterial;
+import br.com.fiap.clinica.modelo.Tratamento;
 
 @ManagedBean
 @ViewScoped
@@ -20,21 +25,47 @@ public class AtendimentoBean implements Serializable {
 	private EntityManager em = new JPAUtil().getEntityManager();
 	private DAO<Atendimento> dao = new DAO<Atendimento>(Atendimento.class, em);
 	private DAO<Agendamento> daoAg = new DAO<Agendamento>(Agendamento.class, em);
+	private DAO<Tratamento>  daoTr = new DAO<Tratamento>(Tratamento.class, em);
+	
+	private List<AtendimentoMaterial> atendimentoMateriais;
+	private List<Tratamento> tratamentos;
 
 	public AtendimentoBean(){
-	//public AtendimentoBean(Long agendamentoId){
-		//Long id = new Long(2);
-		//this.atendimento = new Atendimento();
-		//this.atendimento.setAgendamento(daoAg.buscaPeloId(id));
+	//public AtendimentoBean(Agendamento agendamento){
+		this.atendimento = new Atendimento();
+		this.atendimento.setAgendamento(daoAg.buscaPeloId(new Long(2)));
+		this.tratamentos  = daoTr.lista();
 	}
 	
 	public Atendimento getAtendimento() {
 		return this.atendimento;
 	}
 	
+	public List<Tratamento> getMateriais(){
+		return this.tratamentos;
+	}
+	
+	public Collection<AtendimentoMaterial> getAtendimentoMateriais(){
+		if (this.atendimento.getId() == null) {
+			return new ArrayList();
+		} else {
+			return this.atendimento.getAtendimentoMateriais();
+		}
+	}
+	
+	public void adicionaSelecionado(AtendimentoMaterial item){ 
+		this.atendimentoMateriais.add(item);
+	}
+	
 	public void grava() {
 		try {
 			em.getTransaction().begin();
+
+//			for (int i = 0; i < this.atendimentoMateriais.size(); i++) {
+//				adicionaSelecionado(this.atendimentoMateriais)
+//			}
+			
+			this.atendimento.setAtendimentoMateriais(this.atendimentoMateriais);
 			if (this.atendimento.getId() == null) {
 				dao.adiciona(atendimento);
 			} else {
@@ -42,9 +73,9 @@ public class AtendimentoBean implements Serializable {
 			}
 			em.getTransaction().commit();
 		} catch (Exception e) {
-				em.getTransaction().rollback();
+			em.getTransaction().rollback();
 		}
-		limpaForm();
+//		limpaForm();
 	}
 	
 	public void limpaForm(){
