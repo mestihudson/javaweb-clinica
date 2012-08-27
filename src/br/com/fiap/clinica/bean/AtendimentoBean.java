0d362375aa1f0ffc9494b6d2fb3b218bb65c97e6
gianpaulosoares@gmail.com <gianpaulosoares@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-27 01:11:29 +0000

    Merge branch 'working'

--- src/br/com/fiap/clinica/bean/AtendimentoBean.java
+++ src/br/com/fiap/clinica/bean/AtendimentoBean.java
@@ -1,21 +1,53 @@
 package br.com.fiap.clinica.bean;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+
+import java.io.Serializable;
 
 import br.com.fiap.clinica.dao.DAO;
+import br.com.fiap.clinica.dao.JPAUtil;
+import br.com.fiap.clinica.modelo.Agendamento;
 import br.com.fiap.clinica.modelo.Atendimento;
 
 @ManagedBean
-public class AtendimentoBean {
+@ViewScoped
+public class AtendimentoBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Atendimento atendimento;
+	private EntityManager em = new JPAUtil().getEntityManager();
+	private DAO<Atendimento> dao = new DAO<Atendimento>(Atendimento.class, em);
+	private DAO<Agendamento> daoAg = new DAO<Agendamento>(Agendamento.class, em);
 
-	private Atendimento atendimento = new Atendimento();
+	public AtendimentoBean(){
+	//public AtendimentoBean(Long agendamentoId){
+		//Long id = new Long(2);
+		//this.atendimento = new Atendimento();
+		//this.atendimento.setAgendamento(daoAg.buscaPeloId(id));
+	}
 	
 	public Atendimento getAtendimento() {
 		return this.atendimento;
 	}
 	
 	public void grava() {
-		DAO<Atendimento> dao = new DAO<Atendimento>(Atendimento.class);
-		dao.adiciona(atendimento);
+		try {
+			em.getTransaction().begin();
+			if (this.atendimento.getId() == null) {
+				dao.adiciona(atendimento);
+			} else {
+				dao.atualiza(atendimento);
+			}
+			em.getTransaction().commit();
+		} catch (Exception e) {
+				em.getTransaction().rollback();
+		}
+		limpaForm();
+	}
+	
+	public void limpaForm(){
+		this.atendimento = new Atendimento();
 	}
 }
