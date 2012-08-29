cec1c1481e223f4d595d24635bf2a8b85c492051
edu.merckx@gmail.com <edu.merckx@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-29 00:44:17 +0000

    Iniciando Faturamento

---
+++ src/br/com/fiap/clinica/bean/FaturamentoBean.java
@@ -0,0 +1,10 @@
+package br.com.fiap.clinica.bean;
+
+import java.io.Serializable;
+
+public class FaturamentoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+}
