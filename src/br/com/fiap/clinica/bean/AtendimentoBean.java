e2db2a7b9608b21ca788336d1aa5c7d9995e609b
marsiggia001@gmail.com <marsiggia001@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-29 01:41:34 +0000

    tentando corrigir o atendimento

--- src/br/com/fiap/clinica/bean/AtendimentoBean.java
+++ src/br/com/fiap/clinica/bean/AtendimentoBean.java
@@ -1,13 +1,12 @@
 package br.com.fiap.clinica.bean;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 import br.com.fiap.clinica.dao.DAO;
 import br.com.fiap.clinica.dao.JPAUtil;
@@ -20,48 +19,60 @@ import br.com.fiap.clinica.modelo.Tratamento;
 @ViewScoped
 public class AtendimentoBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
 	private Atendimento atendimento;
 	private EntityManager em = new JPAUtil().getEntityManager();
 	private DAO<Atendimento> dao = new DAO<Atendimento>(Atendimento.class, em);
 	private DAO<Agendamento> daoAg = new DAO<Agendamento>(Agendamento.class, em);
-	private DAO<Tratamento> daoTr = new DAO<Tratamento>(Tratamento.class, em);
+
+	private DAO<Tratamento>  daoTr = new DAO<Tratamento>(Tratamento.class, em);
+	private DAO<AtendimentoMaterial>  daoAtendimentoMaterial = new DAO<AtendimentoMaterial>(AtendimentoMaterial.class, em);	
+	
 	private List<AtendimentoMaterial> atendimentoMateriais;
 	private List<Tratamento> tratamentos;
+	
+	private List<Tratamento> tratamentosSelecionados;
+	
+	
+	private List<String> a;
 
-	public AtendimentoBean() {
-		// public AtendimentoBean(Agendamento agendamento){
+	public AtendimentoBean(){
+	//public AtendimentoBean(Agendamento agendamento){
 		this.atendimento = new Atendimento();
 		this.atendimento.setAgendamento(daoAg.buscaPeloId(new Long(2)));
-		this.tratamentos = daoTr.lista();
+		this.tratamentos  = daoTr.lista();
+		this.tratamentosSelecionados = new ArrayList<Tratamento>();
 	}
-
+	
 	public Atendimento getAtendimento() {
 		return this.atendimento;
 	}
-
-	public List<Tratamento> getMateriais() {
+	
+	public List<Tratamento> getMateriais(){
 		return this.tratamentos;
 	}
-
-	public Collection<AtendimentoMaterial> getAtendimentoMateriais() {
-		if (this.atendimento.getId() == null) {
-			return new ArrayList();
-		} else {
-			return this.atendimento.getAtendimentoMateriais();
-		}
+	
+	public List<Tratamento> getTratamentosSelecionados(){
+		return this.tratamentosSelecionados;
 	}
-
-	public void setAtendimentoMateriais(AtendimentoMaterial item) {
-		this.atendimentoMateriais.add(item);
+	
+	public void setTratamentosSelecionados(List<Tratamento> itensSelecionados){
+		this.tratamentosSelecionados = itensSelecionados;
 	}
-
+	
 	public void grava() {
 		try {
 			em.getTransaction().begin();
 
+
 			// for (int i = 0; i < this.atendimentoMateriais.size(); i++) {
 			// adicionaSelecionado(this.atendimentoMateriais)
 			// }
+
+//			for (int i = 0; i < this.atendimentoMateriais.size(); i++) {
+//				adicionaSelecionado(this.atendimentoMateriais)
+//			}
+
 			this.atendimento.setAtendimentoMateriais(this.atendimentoMateriais);
 			if (this.atendimento.getId() == null) {
 				dao.adiciona(atendimento);
@@ -74,8 +85,8 @@ public class AtendimentoBean implements Serializable {
 		}
 		// limpaForm();
 	}
-
-	public void limpaForm() {
+	
+	public void limpaForm(){
 		this.atendimento = new Atendimento();
 	}
 }
