d99e24bdf0059f590766e91ac6f124ca4b39a44d
edu.merckx@gmail.com <edu.merckx@gmail.com@eede98da-1e4b-123c-ec93-67748d7bbd8c>
2012-08-29 04:10:46 +0000

    Tentando entender..

--- src/br/com/fiap/clinica/bean/FaturamentoBean.java
+++ src/br/com/fiap/clinica/bean/FaturamentoBean.java
@@ -3,20 +3,23 @@ package br.com.fiap.clinica.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import br.com.fiap.clinica.dao.FaturamentoDAO;
 import br.com.fiap.clinica.modelo.Convenio;
 import br.com.fiap.clinica.modelo.Medico;
 
 public class FaturamentoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private FaturamentoDAO dao = new FaturamentoDAO();
 
 	public List<Convenio> getFaturamentoPorConvenio() {
 		
-		return null;
+		return dao.getConveniosFaturados();
 	}
 	
 	public List<Medico> getFaturamentoPorMedico() {
 		
-		return null;
+		return dao.getMedicosFaturados();
 	}
 }
--- src/br/com/fiap/clinica/dao/FaturamentoDAO.java
+++ src/br/com/fiap/clinica/dao/FaturamentoDAO.java
@@ -3,6 +3,7 @@ package br.com.fiap.clinica.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.com.fiap.clinica.modelo.Convenio;
 import br.com.fiap.clinica.modelo.Medico;
@@ -11,9 +12,15 @@ public class FaturamentoDAO {
 
 	private EntityManager em = new JPAUtil().getEntityManager();
 	
-	private List<Convenio> convenios;
+	public List<Convenio> getConveniosFaturados() {
+		
+		Query query = em.createQuery("from Convenio convenio");
+		return query.getResultList();
+	}
 	
-	private List<Medico> medicos;
-	
-	
+	public List<Medico> getMedicosFaturados() {
+		
+		Query query = em.createQuery("from Medico medico");
+		return query.getResultList();
+	}
 }
